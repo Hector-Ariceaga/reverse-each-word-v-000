086e5ca51baf2fa5d8b3f52d783ae87e612724a5
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-21 01:24:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,6 @@ def reverse_each_word(string)
   array = string.split(" ")
   output_array = []
   array.each {|word| output_array << word.reverse}
+  output = output_array.join(" ")
   return output
 end
